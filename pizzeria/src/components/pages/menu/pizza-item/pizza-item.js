280f05bfb2cd61cc9c111b4854a4f785ae6817a8
Jarod Gowgiel <jarod.gowgiel@tufts.edu>
2018-03-08 13:15:48 +0000

    Rollback to pre-tinkering state

--- pizzeria/src/components/pages/menu/pizza-item/pizza-item.js
+++ pizzeria/src/components/pages/menu/pizza-item/pizza-item.js
@@ -1,11 +1,7 @@
-import React from "react";
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { PropTypes } from 'prop-types';
 
-import { Component } from "react";
-import { connect } from "react-redux";
-
-import { addPizza } from "../../../../actions/actions.js";
 
 // This class represents a single pizza, and displays the information from
 // the given object data in a readable format
@@ -14,16 +10,13 @@ import { addPizza } from "../../../../actions/actions.js";
 // that is further interpreted by a reducer to add a pizza to the global store
 // and update the cart state with the new item
 
-const mapDispatchToProps = dispatch => {
-  return {
-    addPizza: PizzaItem => dispatch(addPizza(PizzaItem))
-  };
-};
 
 
 class PizzaItem extends React.Component {
     render() {
+
         var info = this.props.data;
+
         return (
             <div className="card">
                 <img className="card-img-top" src={info.image} alt=""></img>
@@ -32,7 +25,7 @@ class PizzaItem extends React.Component {
                     <h6 className="card-subtitle mb-2 text-muted">Price: {info.price} ISK</h6>
 
                     {/* TODO: Implement the cart and link this button to something*/}
-                    <button className="btn btn-primary mr-1 mb-1" onClick={this.addPizzaToStore(info.id, info.name)}>Add to cart</button>
+                    <button className="btn btn-primary mr-1 mb-1">Add to cart</button>
                     <Link to={"/pizzas/" + info.id} className="btn btn-primary mb-1">More Info</Link>
                 </div>
 
@@ -41,10 +34,6 @@ class PizzaItem extends React.Component {
         )
     }
 
-    addPizzaToStore(id, name)
-    {
-      this.props.addPizza({id:id, name:name});
-    };
 };
 
 
--- pizzeria/src/index.js
+++ pizzeria/src/index.js
@@ -4,17 +4,17 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './components/app/app.js';
-import store from './store.js';
 
 // Import redux dependencies
 import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 // Import misc dependencies
 import registerServiceWorker from './registerServiceWorker';
 
-/* Big TODO here: Make this actually produce a store
+// Big TODO here: Make this actually produce a store
 let store = createStore(function () { return null; })
-*/
+
 
 // Render the BrowserRouter nexted inside a Provider to allow store access
 ReactDOM.render(
